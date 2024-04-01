8aa5f6b9cf33e87160b6b790474a551094bd9113
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-04-01 16:54:03 -0600

    Fix Playlist.jsx and SearchResults.jsx

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -2,7 +2,7 @@ const Playlist = () => {
     return (
         <div className="h-fit w-full bg-purple-400 
         rounded-md bg-clip-padding backdrop-filter 
-        backdrop-blur-md bg-opacity-20 shadow-lg p-4">
+        backdrop-blur-md bg-opacity-30 shadow-lg p-4">
             <h1>Playlist component</h1>
             <p>Here, the selected tracks from Search Results component will show</p>
         </div>
--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -5,10 +5,11 @@ const SearchResults = () => {
         { title: 'Birds', artist: 'Imagine Dragons', album: 'Evolve' },
         { title: 'Hopeless Opus', artist: 'Image Dragons', album: 'Smoke + Mirrors' },
         { title: 'Demons', artist: 'Imagine Dragons', album: 'Night Visions' },
+        { title: 'Thief', artist: 'Image Dragons', album: 'Smoke + Mirrors' },
     ];
 
     return (
-        <div className="search-results p-4 h-fit w-full bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 shadow-lg">
+        <div className="search-results p-4 h-fit w-full bg-purple-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 shadow-lg">
             {trackList.map((track, index) => (
                 <div key={index}>
                     <Track title={track.title} artist={track.artist} album={track.album} />
